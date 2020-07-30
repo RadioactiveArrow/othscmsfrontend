45085090b786cab7856a33b6c6a91ee3c843acca
Sai Charan <tsaicharan03@gmail.com>
2020-07-30 17:35:49 -0500

    Runs tab's functional component works and prints message if status is an error

--- src/components/RunCard.js
+++ src/components/RunCard.js
@@ -1,10 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const RunCard = ({fileName, problemName, status, msg}) => {
+
     return (
         <div>
+
+           {/* returns fileName that is submitted*/}
+           <h1>
+               {fileName}
+           </h1>
+
+            {/*returns problem name*/}
+            <h2>
+                {problemName}
+            </h2>
+
+            {/*checks if status of run contains error*/}
+            {status ="error" &&
+                <h3>{msg}</h3>
+            }
+
         </div>
     )
 }
-
 export default RunCard
+
--- src/components/pages/competitor/Runs.js
+++ src/components/pages/competitor/Runs.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import RunCard from '../../RunCard'
 import '../../../styles/content.css'
 
